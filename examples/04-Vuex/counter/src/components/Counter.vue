5d6a779e4f64b44f1d78725a0c9f82690c3f15bc
Weston Harper <wes@tao.team>
2019-07-29 23:55:23 -0600

    began 4th chapter and added example for installation and early demo

---
+++ examples/04-Vuex/counter/src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <p>{{ count }}</p>
+    <button @click="onClick()">Click Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    onClick() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
