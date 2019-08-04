42754b4fefa0cbdb15532bd5da366b15e06f8892
Weston Harper <wes@tao.team>
2019-08-04 13:06:47 -0600

    finish computed properties and assignment

---
+++ examples/04-Vuex/movie-app/src/components/ComedyMovieList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Comedy</h2>
+    <ul>
+      <li v-for="movie in comedyMovies" :key="movie.id">{{ movie.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComedyMovieList",
+  computed: {
+    comedyMovies() {
+      return this.$store.state.movies.filter(movie => movie.genre === "Comedy");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
