0c5be4aa9fa704c5baf56334dd97f47f8cc2dcc0
Weston Harper <wes@tao.team>
2019-08-05 21:59:48 -0600

    added mutations and example

---
+++ examples/04-Vuex/movie-app-with-mutations/src/components/MovieListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="movie">
+    <h5>{{ movie.title }}</h5>
+    <span class="score">score: {{ movie.score }}</span>
+    <div class="voting-buttons-container">
+      <button @click="onUpvote()">Upvote</button>
+      <button @click="onDownvote()">Downvote</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieListItem",
+  props: {
+    movie: {
+      id: Number,
+      score: Number,
+      title: String,
+      genre: String
+    }
+  },
+  methods: {
+    onUpvote() {
+      this.$store.commit("incrementMovieScore", this.movie.id);
+    },
+    onDownvote() {
+      this.$store.commit("decrementMovieScore", this.movie.id);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.movie {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 40%;
+}
+</style>
