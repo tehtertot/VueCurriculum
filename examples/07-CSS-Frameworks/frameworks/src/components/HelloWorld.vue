889a14526e3b5b63ec77e994b3f0217d3fd9ab56
Weston Harper <wes@tao.team>
2019-08-18 16:17:19 -0600

    added examples and finished 7.1

---
+++ examples/07-CSS-Frameworks/frameworks/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello-world">
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
