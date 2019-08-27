8ee2909d05f3a80d7f6b2c847daada49e8b8ad77
Wes Harper <wes.harper@ibotta.com>
2019-08-26 19:52:23 -0600

    added complete example

---
+++ examples/05-Testing/component-test-example-comp/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="counter">
+    <h1>{{ count }}</h1>
+    <button @click="incrementCount()">Increment Count</button>
+    <button @click="resetCount()">Reset Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    incrementCount() {
+      this.$data.count += 1;
+    },
+    resetCount() {
+      this.$data.count = 0;
+    }
+  }
+};
+</script>
---
+++ examples/05-Testing/component-test-example-comp/tests/unit/counter.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import Counter from "@/components/Counter.vue";
+
+describe("Counter.vue", () => {
+  // tests for initial state will go here
+  // we're not going to make a sub-suite for those
+  describe("increment button", () => {});
+  describe("reset button", () => {});
+});
